ff633c0c56cb11701b5a82b97b64991421070d85
Cynthia Nguyen <cynthia@ool-ad0201be.dyn.optonline.net>
2018-11-19 10:54:16 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.querySelector('.square').onclick = function(){
+
+// }
+document.querySelector('.p3').addEventListener("click", display)
+
+
+function display(){
+  document.querySelector('.projects-page').style.opacity = "1"
+  // console.log(document.querySelector('.projects-page').style)
+}
+
