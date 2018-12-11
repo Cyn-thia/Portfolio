00e24ca923a09bc9a49408b8f03736655a792684
Cynthia Nguyen <cynthia@Cynthias-MacBook-Pro.local>
2018-12-11 06:09:36 -0500

    completed landing and functionality

--- script.js
+++ script.js
@@ -1,11 +1,65 @@
 // document.querySelector('.square').onclick = function(){
 
 // }
-document.querySelector('.p3').addEventListener("click", display)
+document.querySelector('.logo').addEventListener("click", togglePanels)
+
+document.querySelectorAll('.pane').forEach(function(elem){
+  elem.addEventListener("click", toggleActive)
+})
 
 
-function display(){
-  document.querySelector('.projects-page').style.opacity = "1"
-  // console.log(document.querySelector('.projects-page').style)
+function toggleActive(e){
+  console.log(e.target.classList.contains('active'))
+  e.target.classList.contains('active') ?
+    e.target.classList.remove('active') :
+    e.target.classList.add('active')
 }
+
+function togglePanels(){
+  if(document.querySelector('.p1').classList.contains('show')) {
+    hidePanels()
+  } else {
+    showPanels()
+  }
+}
+
+function showPanels() {
+  toggleCloudsLanding()
+  console.log('show')
+  const elems = document.querySelectorAll(".p1, .p2, .p3, .p4")
+    for (i = 0; i<elems.length; i++){
+    elems[i].classList.add('show')
+    }
+
+}
+
+function hidePanels(e) {
+  console.log('hide')
+  toggleCloudsLanding()
+  const elems = document.querySelectorAll(".p1, .p2, .p3, .p4")
+    for (i = 0; i<elems.length; i++){
+    elems[i].classList.remove('show')
+    }
+}
+
+function toggleCloudsLanding(){
+  const elems = document.querySelectorAll(".landing-sect, .clouds")
+  const headers = document.querySelectorAll('.header')
+  if(document.querySelector('.landing-sect').style.display !== 'none'){
+    for (i = 0; i<elems.length; i++){
+    elems[i].style.display = 'none'
+    }
+    for (i = 0; i<headers.length; i++){
+    headers[i].style.display = 'block'
+    }
+  } else {
+    for (i = 0; i<elems.length; i++){
+    elems[i].style.display = 'block'
+    }
+    for (i = 0; i<headers.length; i++){
+    headers[i].style.display = 'none'
+    }
+  }
+}
+
 
